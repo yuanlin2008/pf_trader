56461d313719e3b6d626350d643df35fa2a5183f
yuanlin2008 <yuanlin2008@hotmail.com>
2026-03-01 11:42:27 +0800

    123

--- src/pf_trader/trader.py
+++ src/pf_trader/trader.py
@@ -1,3 +1,39 @@
+from dataclasses import dataclass
+from typing import Callable
 
-def run():
-    pass
+import pandas as pd
+
+
+@dataclass
+class History:
+    """
+    策略执行的历史数据
+    """
+
+    values: pd.DataFrame
+    positions: pd.DataFrame
+
+
+def run(
+    strategy: Callable,
+    prices: pd.DataFrame,
+    history: History | None,
+):
+    """
+    Run the trading strategy.
+    """
+    market_start = prices["date"].min()
+    market_end = prices["date"].max()
+    if history is None:
+        history = History(
+            values=pd.DataFrame(
+                {
+                    "date": [market_start],
+                    "value": [1.0],
+                }
+            ),
+            positions=pd.DataFrame(columns=["date", "instrument", "value", "weight"]),
+        )
+
+    for date, date_prices in prices.groupby("date"):
+        pass
