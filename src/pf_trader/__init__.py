492910168cd2787f4defd6871c36ccda91d70a07
yuanlin2008 <yuanlin2008@hotmail.com>
2026-03-02 15:48:21 +0800

    123

--- src/pf_trader/__init__.py
+++ src/pf_trader/__init__.py
@@ -1,7 +1,6 @@
 """pf_trader - 量化交易回测框架"""
 
-from pf_trader.trader import History, run
+from pf_trader.trader import Context, History, run
 
 __version__ = "0.1.0"
-__all__ = ["History", "run"]
-
+__all__ = ["History", "run", "Context"]
